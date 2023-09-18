7f46eabcc53d779aeb48889328d2041a8f5ac0dc
Aditya <aditya.gupta@thinkitive.com>
2023-09-19 03:16:58 +0530

    feat : add the react vite initial skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# REACT_UI_COMPONENT
+This repository is for provider portal UI of TRST Health
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppClass from "./app.module.scss";
+
+function App() {
+  return (
+    <div className={AppClass.app}>
+      <h1> Welcome to React UI component</h1>
+    </div>
+  );
+}
+
+export default App;
